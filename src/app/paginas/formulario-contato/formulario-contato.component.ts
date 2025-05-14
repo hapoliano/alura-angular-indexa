591d49e0a6b8874822e8c9ceaf608a8214af8941
Daniel Apoliano <danielapoliano02@gmail.com>
2025-05-14 14:17:47 -0300

    Aplicando as diretivas

--- src/app/paginas/formulario-contato/formulario-contato.component.ts
+++ src/app/paginas/formulario-contato/formulario-contato.component.ts
@@ -30,4 +30,12 @@ export class FormularioContatoComponent {
       observacoes: new FormControl('')
     })
   }
+
+  salvarContato() {
+    console.log(this.contatoForm.value)
+  }
+
+  cancelar() {
+    console.log("Submissão cancelada")
+  }
 }
