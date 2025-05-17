0fadf782cb37fa02f7cb24cd7749f105ec992726
Daniel Apoliano <danielapoliano02@gmail.com>
2025-05-17 15:32:48 -0300

    Obtendo parâmetros das rotas

--- src/app/paginas/formulario-contato/formulario-contato.component.ts
+++ src/app/paginas/formulario-contato/formulario-contato.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Router, RouterLink } from '@angular/router'
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 import { ContainerComponent } from '../../componentes/container/container.component';
 import { SeparadorComponent } from '../../componentes/separador/separador.component';
@@ -15,22 +20,23 @@ import { ContatoService } from '../../services/contato.service';
     ContainerComponent,
     SeparadorComponent,
     ReactiveFormsModule,
-    RouterLink
+    RouterLink,
   ],
   templateUrl: './formulario-contato.component.html',
-  styleUrl: './formulario-contato.component.css'
+  styleUrl: './formulario-contato.component.css',
 })
-export class FormularioContatoComponent implements OnInit{
-
+export class FormularioContatoComponent implements OnInit {
   contatoForm!: FormGroup;
 
   constructor(
     private contatoService: ContatoService,
-    private router: Router
-    ) {}
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit() {
     this.inicializarFormulario();
+    this.carregarContato();
   }
 
   inicializarFormulario() {
@@ -40,17 +46,29 @@ export class FormularioContatoComponent implements OnInit{
       email: new FormControl('', [Validators.required, Validators.email]),
       aniversario: new FormControl(''),
       redes: new FormControl(''),
-      observacoes: new FormControl('')
-    })
+      observacoes: new FormControl(''),
+    });
   }
 
+  carregarContato() {
+       const id = this.activatedRoute.snapshot.paramMap.get('id');
+       if (id) {
+           this.contatoService.buscarPorId(parseInt(id)).subscribe((contato) => {
+               this.contatoForm.patchValue(contato)
+           });
+       }
+   }
+
   salvarContato() {
-    const novoContato = this.contatoForm.value;
-    this.contatoService.salvarContato(novoContato).subscribe(() =>{
-      this.contatoForm.reset();
-      this.router.navigateByUrl('/lista-contatos')
-    })
-  }
+        const novoContato = this.contatoForm.value;
+        const id = this.activatedRoute.snapshot.paramMap.get('id');
+        novoContato.id = id ? parseInt(id) : null
+        
+        this.contatoService.editarOuSalvarContato(novoContato).subscribe(() => {
+            this.contatoForm.reset();
+            this.router.navigateByUrl('/lista-contatos')
+        });
+    }
 
   cancelar() {
     this.contatoForm.reset();
